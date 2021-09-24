3a2e98fe674356b3a972361f02cdf47e6db3be40
bleak.twig <bruno.benkel@gmail.com>
2021-09-24 11:56:02 -0300

    Finalized run script

--- fmt/run.sh
+++ fmt/run.sh
@@ -3,11 +3,11 @@
 # Setup (Point these to the appropiate location)
 TORUSMAP="Symm_torus_r2501_phi16_z251_24Apr2018.dat"
 SOLENOIDMAP="Symm_solenoid_r601_phi1_z1201_13June2018.dat"
-MVN="/home/twig/data/code/jsw/netbeans-12.0/netbeans/java/maven/bin/mvn"
+MVN="/path/to/mvn"
 JAVAHOME="/usr"
-COATJAVA="/home/twig/data/code/jsw/coatjava-5.6.8"
+COATJAVA="/path/to/coatjava-5.6.8"
 
-# Don't touch these!
+# Don't touch anything from this point forward!
 JAVALOC="$JAVAHOME/bin/java"
 MAGFIELDSETUP="COAT_MATFIELD_TORUSMAP=$TORUSMAP COAT_MAGFIELD_SOLENOIDMAP=$SOLENOIDMAP"
 DEXECARGS="-Dexec.args=-DCLAS12DIR=$COATJAVA"
@@ -19,9 +19,7 @@ POSTARGS="-Dexec.executable=$DEXECEXECUTABLE process-classes org.codehaus.mojo:e
 
 i="0"
 args=""
-for arg
-    do args="$args $arg"
-done
+for arg; do args="$args $arg"; done
 
 # Run
 export COAT_MAGFIELD_TORUSMAP="$TORUSMAP"
