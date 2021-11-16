44abd9d8cfefea8081bac199a199aad9ab92cf90
Raffaella De Vita <devita@ge.infn.it>
2021-11-17 00:00:44 +0100

    complete treatment of residuals

--- dc/java/dc-alignment/src/main/java/org/clas/dc/alignment/Alignment.java
+++ dc/java/dc-alignment/src/main/java/org/clas/dc/alignment/Alignment.java
@@ -8,12 +8,15 @@ import java.util.List;
 import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
+import org.jlab.detector.calib.utils.ConstantsManager;
+import org.jlab.geom.prim.Vector3D;
 import org.jlab.groot.data.GraphErrors;
 import org.jlab.groot.data.TDirectory;
 import org.jlab.groot.graphics.EmbeddedCanvasTabbed;
 import org.jlab.groot.graphics.EmbeddedPad;
 import org.jlab.groot.group.DataGroup;
 import org.jlab.jnp.utils.options.OptionStore;
+import org.jlab.utils.groups.IndexedTable;
 
 /**
  *
@@ -27,12 +30,27 @@ public class Alignment {
     private String[]          inputs = new String[19];
     private Bin[]             thetaBins = null;
     private Bin[]             phiBins  = null;
+    private ConstantsManager  manager = new ConstantsManager();
+    private IndexedTable      alignment = null;
+    
+    private double            unitShift = 0.2;
     
     public Alignment() {
-        this.initsInputs();
+        this.initInputs();
     }
     
-    private void initsInputs() {
+    private void initConstants(int run, String variation) {
+        if(!variation.isEmpty()) {
+            String table = "/geometry/dc/alignment";
+            List<String> tables = new ArrayList<>();
+            tables.add(table);
+            manager.init(tables);
+            manager.setVariation(variation);
+            alignment = manager.getConstants(run, table);
+        }
+    }
+    
+    private void initInputs() {
         String[] inputs = new String[19];
         String[] coord = {"x", "y", "z"};
         inputs[0] = "nominal";
@@ -55,6 +73,95 @@ public class Alignment {
             System.out.println("\nAnalyzing histos for variation " + key);
             histos.get(key).analyzeHisto();
         }
+    }
+    
+    private double getResidualShift(String key, int layer, int itheta, int iphi) {
+        double shift=0;
+        int il = layer - 1;
+        if(histos.containsKey(key)) {
+            for(int is=0; is<nSector; is++) {
+                int sector = is+1;
+                shift += (histos.get(key).getResidualValues(sector, itheta, iphi)[il]-
+                          histos.get("nominal").getResidualValues(sector, itheta, iphi)[il])/nSector;
+            }
+        }
+        return shift;
+    }
+   
+    private double[] getResidualShift(String key, int itheta, int iphi) {
+        double[] shift = new double[nLayer];
+        for(int il=0; il<nLayer; il++) {
+           shift[il] = this.getResidualShift(key, il+1, itheta, iphi);
+        }
+        return shift;
+    }
+    
+    private double getVertexShift(String key, int itheta, int iphi) {
+        double shift=0;
+        if(histos.containsKey(key)) {
+            for(int is=0; is<nSector; is++) {
+                int sector = is+1;
+                shift += (histos.get(key).getVertexValues(sector, itheta, iphi)-
+                          histos.get("nominal").getVertexValues(sector, itheta, iphi))/nSector;
+            }
+        }
+        return shift;
+    }
+    
+    private double getFittedResidual(int sector, int layer, int itheta, int iphi) {
+        double value=0;
+        int il = layer - 1;
+        for(String key : histos.keySet()) {
+            if(!key.equals("nominal")) 
+                value += histos.get(key).getResidualValues(sector, itheta, iphi)[il]*
+                         this.getShiftSize(key, sector, layer)/this.unitShift;
+        }
+        return value;
+    }
+
+    private double[] getFittedResidual(int sector, int itheta, int iphi) {
+        double[] shift = new double[nLayer];
+        for(int layer=1; layer<=nLayer; layer++) {
+           shift[layer-1] = this.getFittedResidual(sector, layer, itheta, iphi);
+        }
+        return shift;
+    }
+    
+    private double getShiftSize(String key, int sector, int layer) {
+        double shift = 0;
+        int region = (layer-1)/12 + 1;
+        if(key.contains("_c")) {  // rotation
+            String axis = key.split("_c")[1];
+            shift = this.alignment.getDoubleValue("dtheta_" + axis, region, sector, 0);
+//            System.out.println("dtheta_" + axis +  " " + region + " " + sector + " " + shift);
+        }
+        else { // offset
+            Vector3D offset = new Vector3D(this.alignment.getDoubleValue("dx", region, sector, 0),
+                                           this.alignment.getDoubleValue("dy", region, sector, 0),
+                                           this.alignment.getDoubleValue("dz", region, sector, 0));
+            offset.rotateZ(Math.toRadians(-60*(sector-1)));
+            if(layer==1) {
+                Vector3D test = new Vector3D(0,0.2,0);
+                test.rotateZ(Math.toRadians(60*(sector-1)));
+                System.out.println("d" + " " + sector + " " + test.toString());
+            }
+            String axis = key.split("_")[1];
+            switch (axis) {
+                case "x":
+                    shift = offset.x();
+                    break;
+                case "y":
+                    shift = offset.y();
+                    break;
+                case "z":
+                    shift = offset.z();
+                    break;
+                default:
+                    break;
+            }
+//            System.out.println("d" + axis +  " " + region + " " + sector + " " + shift);
+        }
+        return shift;
     }
     
     public EmbeddedCanvasTabbed analyzeFits() {
@@ -81,6 +188,20 @@ public class Alignment {
                     gr_res.setTitleY("Layer");
                     gr_res.setMarkerColor((ip-1)*thetaBins.length+it+1);
                     nomResiduals.addDataSet(gr_res, is);
+                    if(alignment!=null) {
+                        GraphErrors gr_fit = new GraphErrors("gr_fit_S" + sector + "_theta " + it + "_phi" + ip, 
+                                                             this.getFittedResidual(sector,it,ip), 
+                                                             layers, 
+                                                             zeros, 
+                                                             zeros);
+                        gr_fit.setTitle("Sector " + sector);
+                        gr_fit.setTitleX("Residual (um)");
+                        gr_fit.setTitleY("Layer");
+                        gr_fit.setMarkerColor((ip-1)*thetaBins.length+it+1);
+                        gr_fit.setMarkerStyle(2);
+                        nomResiduals.addDataSet(gr_fit, is);
+                        System.out.println("fitting");
+                    }
                 }               
             }
         }
@@ -121,6 +242,12 @@ public class Alignment {
                             errorVtx[is] = Math.sqrt(Math.pow(shiftedHisto.getVertexErrors(sector, it, ip),2)+
                                                      Math.pow(nomHisto.getVertexErrors(sector, it, ip),2));
                         }
+                        GraphErrors gr_res = new GraphErrors("gr_res" + "_theta " + it + "_phi" + ip, this.getResidualShift(key, it, ip), layers, zeros, zeros);
+                        gr_res.setTitle("Theta:"+ thetaBins[it].getRange() + " Phi:" + phiBins[ip].getRange());
+                        gr_res.setTitleX("Residual (um)");
+                        gr_res.setTitleY("Layer");
+                        gr_res.setMarkerColor(1);
+                        shiftedResiduals.addDataSet(gr_res, ip*thetaBins.length+it);
                         GraphErrors gr_vtx = new GraphErrors("gr_vtx_theta " + it + "_phi" + ip, shiftVtx, layers, errorVtx, zeros);
                         gr_vtx.setTitle("Theta:"+ thetaBins[it].getRange() + " Phi:" + phiBins[ip].getRange());
                         gr_vtx.setTitleX("Vertex (cm)");
@@ -135,9 +262,13 @@ public class Alignment {
                     pad.getAxisX().setRange(-2000, 2000);
                 canvas.addCanvas(key + " Vertex");
                 canvas.getCanvas(key + " Vertex").draw(shiftedVertex);
+                for(int ipad=0; ipad<canvas.getCanvas(key + " Vertex").getCanvasPads().size(); ipad++) {
+                    double average = getVertexShift(key,ipad%thetaBins.length,(int) (ipad/thetaBins.length));
+                    canvas.getCanvas(key + " Vertex").getPad(ipad).getAxisX().setRange(average-1, average+1);
+                }
             }
         }
-        this.printResults();
+//        this.printResults();
         return canvas;
     }
     
@@ -167,23 +298,9 @@ public class Alignment {
                     for(int ip=1; ip<phiBins.length; ip++) {
                         System.out.print("{");
                         for(int il=0; il<nLayer; il++) {
-                            double shift=0;
-                                if(histos.containsKey(key)) {
-                                for(int is=0; is<nSector; is++) {
-                                    int sector = is+1;
-                                    shift += (histos.get(key).getResidualValues(sector, it, ip)[il]-histos.get("nominal").getResidualValues(sector, it, ip)[il])/nSector;
-                                }
-                            }
-                            System.out.print(String.format("%.1f, ", shift));
+                            System.out.print(String.format("%.1f, ", getResidualShift(key,il+1,it,ip)));
                         }
-                        double shift=0;
-                        if(histos.containsKey(key)) {
-                            for(int is=0; is<nSector; is++) {
-                                int sector = is+1;
-                                shift += (histos.get(key).getVertexValues(sector, it, ip)-histos.get("nominal").getVertexValues(sector, it, ip))*1E4/nSector;
-                            }
-                        }
-                        System.out.print(String.format("%.1f}", shift));
+                        System.out.print(String.format("%.1f}", getVertexShift(key,it,ip)));
                         if(!(it==thetaBins.length-1 && ip==phiBins.length-1)) System.out.println(",");
                     }
                 }
@@ -290,22 +407,24 @@ public class Alignment {
         
         // valid options for event-base analysis
         parser.addCommand("-process", "process event files");
-        parser.getOptionParser("-process").addOption("-o"       ,"",   "output file name prefix");
-        parser.getOptionParser("-process").addOption("-nevent"  ,"-1", "maximum number of events to process");
-        parser.getOptionParser("-process").addRequired("-" + inputs[0],"nominal geometry files");
+        parser.getOptionParser("-process").addOption("-o"        ,"",              "output file name prefix");
+        parser.getOptionParser("-process").addOption("-nevent"   ,"-1",            "maximum number of events to process");
+        parser.getOptionParser("-process").addRequired("-" + inputs[0],            "nominal geometry files");
         for(int i=1; i<inputs.length; i++) parser.getOptionParser("-process").addOption("-" + inputs[i],"");
-        parser.getOptionParser("-process").addOption("-display" ,"1",  "display histograms (0/1)");
-        parser.getOptionParser("-process").addOption("-stats"   ,"",   "set histogram stat option");
-        parser.getOptionParser("-process").addOption("-theta"   , "",  "theta bin limits, e.g. \"5:10:20:30\"");
-        parser.getOptionParser("-process").addOption("-phi"     , "",  "phi bin limits, e.g. \"-30:-15:0:15:30\"");
+        parser.getOptionParser("-process").addOption("-display"  ,"1",             "display histograms (0/1)");
+        parser.getOptionParser("-process").addOption("-stats"    ,"",              "set histogram stat option");
+        parser.getOptionParser("-process").addOption("-theta"    , "",             "theta bin limits, e.g. \"5:10:20:30\"");
+        parser.getOptionParser("-process").addOption("-phi"      , "",             "phi bin limits, e.g. \"-30:-15:0:15:30\"");
+        parser.getOptionParser("-process").addOption("-variation", "",             "database variation for constant test");
         
         // valid options for histogram-base analysis
         parser.addCommand("-analyze", "analyze histogram files");
-        parser.getOptionParser("-analyze").addRequired("-input" ,      "input histogram file");
-        parser.getOptionParser("-analyze").addOption("-display" ,"1",  "display histograms (0/1)");
-        parser.getOptionParser("-analyze").addOption("-stats"   ,"",   "set histogram stat option");
-        parser.getOptionParser("-analyze").addOption("-theta"   , "",  "theta bin limits, e.g. \"5:10:20:30\"");
-        parser.getOptionParser("-analyze").addOption("-phi"     , "",  "phi bin limits, e.g. \"-30:-15:0:15:30\"");
+        parser.getOptionParser("-analyze").addRequired("-input"  ,                 "input histogram file");
+        parser.getOptionParser("-analyze").addOption("-display"  ,"1",             "display histograms (0/1)");
+        parser.getOptionParser("-analyze").addOption("-stats"    ,"",              "set histogram stat option");
+        parser.getOptionParser("-analyze").addOption("-theta"    , "",             "theta bin limits, e.g. \"5:10:20:30\"");
+        parser.getOptionParser("-analyze").addOption("-phi"      , "",             "phi bin limits, e.g. \"-30:-15:0:15:30\"");
+        parser.getOptionParser("-analyze").addOption("-variation", "",             "database variation for constant test");
         
         parser.parse(args);
         
@@ -322,10 +441,12 @@ public class Alignment {
             String thetaBins   = parser.getOptionParser("-process").getOption("-theta").stringValue();
             String phiBins     = parser.getOptionParser("-process").getOption("-phi").stringValue();
             String optStats    = parser.getOptionParser("-process").getOption("-stats").stringValue();
+            String variation   = parser.getOptionParser("-process").getOption("-variation").stringValue();
             openWindow         = parser.getOptionParser("-process").getOption("-display").intValue()!=0;
             if(!openWindow) System.setProperty("java.awt.headless", "true");
 
             align.setAngularBins(thetaBins, phiBins);
+            align.initConstants(11, variation);
             for(int i=0; i<inputs.length; i++) {
                 String input = parser.getOptionParser("-process").getOption("-" + inputs[i]).stringValue();
                 if(!input.isEmpty()) {                    
@@ -341,11 +462,13 @@ public class Alignment {
             String thetaBins   = parser.getOptionParser("-analyze").getOption("-theta").stringValue();
             String phiBins     = parser.getOptionParser("-analyze").getOption("-phi").stringValue();
             String optStats    = parser.getOptionParser("-analyze").getOption("-stats").stringValue();
+            String variation   = parser.getOptionParser("-analyze").getOption("-variation").stringValue();
             openWindow         = parser.getOptionParser("-analyze").getOption("-display").intValue()!=0;
             if(!openWindow) System.setProperty("java.awt.headless", "true");
 
             String histoName   = parser.getOptionParser("-analyze").getOption("-input").stringValue();
             align.setAngularBins(thetaBins, phiBins);
+            align.initConstants(11, variation);
             align.readHistos(histoName,align.getThetaBins(),align.getPhiBins(),optStats);
             align.analyzeHistos();
         }
